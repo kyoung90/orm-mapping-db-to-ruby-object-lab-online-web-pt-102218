79f4955af275d1b3cf00f86308b8f997543bd3dc
Kenneth Young Castro <kenneth.young90@gmail.com>
2019-01-20 01:01:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -45,6 +45,10 @@ class Student
     end
   end
 
+  def self.first_X_students_in_grade_10(x)
+    rows = DB[:conn].execute("SELECT * FROM students WHERE grade='10' LIMIT ?", x)
+  end 
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
