a0e1f91f4d4b910be42136995f6b5b40435867be
Kenneth Young Castro <kenneth.young90@gmail.com>
2019-01-20 01:04:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,7 +52,7 @@ class Student
     end
   end
 
-  def self.first_X_students_in_grade_10
+  def self.first_student_in_grade_10
     rows = DB[:conn].execute("SELECT * FROM students WHERE grade='10' LIMIT 1")
     rows.map do |row|
       self.new_from_db(row)
